8e24fff3d16102e0919c3dfab37a7547f7b95b2f
Touseef Ahmad <touseef.ahmad.cs@gmail.com>
2020-02-21 11:21:55 +0500

    user can add working hours

--- work_diary/views.py
+++ work_diary/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render , redirect
-
+from django.contrib import messages
+from .models import WorkDiary
 # Create your views here.
 
 def index(request):
     if request.user.is_authenticated:
-        return render(request , 'home.html',{})
+        if request.method == "GET":
+            work_diary = WorkDiary.objects.filter(user=request.user)
+            return render(request , 'home.html',{'user_name': request.user.first_name,'work_diary':work_diary})
+    
+        elif request.method == "POST":
+            hours = request.POST['hours']
+            date = request.POST['date']
+            work_diary = WorkDiary.objects.create(user=request.user , date=date , hours_worked=hours)
+            return redirect('/')
     else:
         return redirect('/users/login')
